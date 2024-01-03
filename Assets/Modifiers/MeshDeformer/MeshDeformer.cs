74b3f42ac73f088a39bbef51d5eb78bced5372b5
Kyle Bauer <kbau121@seas.upenn.edu>
2024-01-02 23:11:30 -0500

    Finished the basic mesh deformer - Presses into the object based on mouse input

--- Assets/Modifiers/MeshDeformer/MeshDeformer.cs
+++ Assets/Modifiers/MeshDeformer/MeshDeformer.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class MeshDeformer : MonoBehaviour
 {
     public float springForce = 20f;
+    public float dampingForce = 5f;
 
     Mesh deformingMesh;
     Vector3[] originalVertices, displacedVertices;
     Vector3[] vertexVelocities;
+
+    float uniformScale = 1f;
 
     void Start()
     {
@@ -27,6 +30,8 @@ public class MeshDeformer : MonoBehaviour
 
     private void Update()
     {
+        uniformScale = transform.localScale.x;
+
         for (int i = 0; i < displacedVertices.Length; ++i)
         {
             UpdateVertex(i);
@@ -40,16 +45,20 @@ public class MeshDeformer : MonoBehaviour
     {
         Vector3 velocity = vertexVelocities[i];
         Vector3 displacement = displacedVertices[i] - originalVertices[i];
+        displacement *= uniformScale;
 
         velocity -= displacement * springForce * Time.deltaTime;
+        velocity *= 1f - (dampingForce * Time.deltaTime);
         vertexVelocities[i] = velocity;
 
-        displacedVertices[i] += velocity * Time.deltaTime;
+        displacedVertices[i] += velocity * (Time.deltaTime / uniformScale);
     }
 
     public void AddDeformingForce(Vector3 point, float force)
     {
         Debug.DrawLine(Camera.main.transform.position, point);
+
+        point = transform.InverseTransformPoint(point);
 
         for (int i = 0; i < displacedVertices.Length; ++i)
         {
@@ -60,6 +69,8 @@ public class MeshDeformer : MonoBehaviour
     void AddForceToVertex(int i, Vector3 point, float force)
     {
         Vector3 pointToVertex = displacedVertices[i] - point;
+        pointToVertex *= uniformScale;
+
         float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
         float dSpeed = attenuatedForce * Time.deltaTime;
 
